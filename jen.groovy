28e13bd095e4fc370adcfeda1b4449156590fad9
reddy0716 <asreddy1607@gmail.com>
2025-08-25 14:05:17 -0600

    Update jen.groovy

--- jen.groovy
+++ jen.groovy
@@ -2,14 +2,14 @@ stage('Debug Namespaces') {
   steps {
     container('python') {
       sh '''
-        echo "Checking available namespaces for PRD instance..."
-        curl -sk -X POST "https://cgrptmcip01.cloud.cammis.ca.gov/api/graphql" \
-          -H "Content-Type: application/json" \
-          -H "x-auth-token: ${TOKEN}" \
-          -d '{
-            "query":"query($id:Int!){ instance(id:$id){ namespaces { id name } } }",
-            "variables":{"id":1}
-          }' | jq .
+       curl -sk -X POST "https://cgrptmcip01.cloud.cammis.ca.gov/api/graphql" \
+  -H "Content-Type: application/json" \
+  -H "x-auth-token: ${TOKEN}" \
+  -d '{
+    "query":"query($id: Long!){ instance(id:$id){ namespaces { id name } } }",
+    "variables":{"id":1}
+  }'
+
       '''
     }
   }
