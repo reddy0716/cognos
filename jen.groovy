01debb7df3137f4d61033b3fa9c381a0e0009a9d
reddy0716 <asreddy1607@gmail.com>
2025-09-10 19:42:46 -0600

    Update jen.groovy

--- jen.groovy
+++ jen.groovy
@@ -1,11 +1,14 @@
-query getInstanceAndNamespaceInformation {
-  instances {
-    edges {
-      node {
-        id
-        name
-        namespaces { id }
-      }
-    }
+[
+  {
+    "instanceId": "1",
+    "namespaceId": "LDAP",
+    "username": "prd_ldap_user",
+    "password": "prd_ldap_password"
+  },
+  {
+    "instanceId": "3",
+    "namespaceId": "LDAP",
+    "username": "devtest_ldap_user",
+    "password": "devtest_ldap_password"
   }
-}
+]
