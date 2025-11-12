648f651923384c5c447a36a169e16bbbe2b3cce0
reddy0716 <asreddy1607@gmail.com>
2025-11-12 14:05:54 -0600

    Replace script execution with file existence check

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,5 +1,1 @@
-def p = ["bash","-c","cd /var/lib/jenkins/workspace/MotioCI/api/CLI && python3 ci-cli.py --help"].execute()
-def out = new StringBuffer()
-p.consumeProcessOutput(out,new StringBuffer())
-p.waitFor()
-println out
+println new File("/var/lib/jenkins/workspace/MotioCI/api/CLI/ci-cli.py").exists()
